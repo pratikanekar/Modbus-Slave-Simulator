525b3cb6d33055776a7c2884e27f0e635c02f74e
Pratik Anekar <pratik.a@smart-iam.com>
2023-01-25 15:58:12 +0530

    redis part added! and solve other problems

--- create_mod_slave.py
+++ create_mod_slave.py
@@ -5,17 +5,23 @@ import redis
 import socket
 from os import getcwd
 
-# random_open = {"01": "coils", "02": "discrete_input", "03": "holding", "04": "input"}
+#following dict is used to comparison of values through csv file values
+random_open_for_csv = {"01": "coils", "02": "discrete_input", "03": "holding", "04": "input"}
 
+# following dict is used to comparison of values through redis database
+random_open_for_redis = {"01": "c", "02": "discrete_input", "03": "h", "04": "i"}
 
-random_open = {"01": "c", "02": "discrete_input", "03": "h", "04": "i"}
-
+# following dict is used to store all value of csv file
 all_register_dict = {"holding": {}, "input": {}, "coils": {}}
+
+flag_for_csv = 0
 
 
 # here store all csv file data into all_register_dict
 def read_csv_file():
+    global flag_for_csv
     try:
+        flag_for_csv = 1
         for file_name in all_register_dict:
             file_open = open(f"{getcwd()}/modbus/{file_name}.csv", "r")
             file_reader = csv.reader(file_open)
@@ -23,64 +29,44 @@ def read_csv_file():
                 all_register_dict.get(file_name).update({row[0]: row[1]})
             file_open.close()
         logger.info(f"Done with File Importing........")
+
     except Exception as e:
         logger.error(f"error occurred in read_csv_file as : {e}")
 
 
-# here store all redis database data into all_register_dict
-def redis_database(function_code, start_address):
-    try:
-        redis_ip = "192.168.1.205"
-        redis_port = 6379
-        start_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        start_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        redis_db_conn = redis.Redis(host=redis_ip, port=redis_port)
-        if redis_db_conn.ping():
-            logger.debug(f"Done connection with Redis Database IP:{redis_ip} Port:{redis_port}")
-            reg_values = redis_db_conn.get(f"{random_open[function_code]}_{start_address}")
-            return reg_values
-
-    except Exception as e:
-        logger.error(f"error occurred in redis_database_function as : {e}")
-
-
-
-# following function is used to finding values of starting addresses
+# following function is used to finding values of starting addresses from redis database
 def find_start_add_value_from_redis(st_add, function_code, mod_count):
     try:
         redis_ip = "192.168.1.205"
         redis_port = 6379
         start_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         start_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        redis_db_conn = redis.Redis(host=redis_ip, port=redis_port)
+        redis_db_conn = redis.Redis(host=redis_ip, port=redis_port, db=1)
         if redis_db_conn.ping():
             logger.debug(f"Done connection with Redis Database IP:{redis_ip} Port:{redis_port}")
         start_address = int(f"{st_add}", 16)
-        # start_address = int(f"0x{st_add}", 0)
         addresses = []
-        for row in all_register_dict:
-            if row == random_open.get(function_code):
+        for row in random_open_for_redis.values():
+            if row == random_open_for_redis.get(function_code):
                 final_count = int(f"0x{mod_count}", 0)
                 for i in range(final_count):
-                    reg_values = redis_database(function_code, start_address)
-                    # reg_values = redis_db_conn.get(f"{random_open[function_code]}_{start_address}")
-                    # reg_values = int(all_register_dict.get(row).get(str(start_address)))
+                    reg_values = int(redis_db_conn.get(f"{random_open_for_redis[function_code]}_{start_address}").decode())
                     start_address = start_address + 1
                     addresses.append(hex(reg_values).replace("0x", ""))
         final_address = ("".join(addresses))
         return final_address
     except Exception as e:
-        logger.error(f"error occurred in find_start_add_value as : {e}")
+        logger.error(f"error occurred in find_start_add_value_from_redis as : {e}")
 
 
-# following function is used to finding values of starting addresses
-def find_start_add_value(st_add, function_code, mod_count):
+# following function is used to finding values of starting addresses for csv file
+def find_start_add_value_from_csv(st_add, function_code, mod_count):
     try:
         start_address = int(f"{st_add}", 16)
         # start_address = int(f"0x{st_add}", 0)
         addresses = []
         for row in all_register_dict:
-            if row == random_open.get(function_code):
+            if row == random_open_for_csv.get(function_code):
                 final_count = int(f"0x{mod_count}", 0)
                 for i in range(final_count):
                     reg_values = int(all_register_dict.get(row).get(str(start_address)))
@@ -89,35 +75,45 @@ def find_start_add_value(st_add, function_code, mod_count):
         final_address = ("".join(addresses))
         return final_address
     except Exception as e:
-        logger.error(f"error occurred in find_start_add_value as : {e}")
+        logger.error(f"error occurred in find_start_add_value_from_csv as : {e}")
 
 
 # following function is used to find bytecount of modbus
-def find_count_register(mod_count, funtion_code):
+def find_byte_count_register(mod_count, funtion_code):
     try:
         if funtion_code == "01" or funtion_code == "02":
             temp = int(f"0x{mod_count}", 0)
             require_byte = ceil(temp / 8) + 1
-            count_val = hex(require_byte).replace("x", "")
+            if require_byte <= 15:
+                count_val = hex(require_byte).replace("x", "")
+            else:
+                count_val = hex(require_byte).replace("0x", "")
             return count_val
         elif funtion_code == "03" or funtion_code == "04":
             temp = int(f"0x{mod_count}", 0)
             require_byte = ceil((temp * 16) / 8)
-            count_val = hex(require_byte).replace("x", "")
+            if require_byte <= 15:
+                count_val = hex(require_byte).replace("x", "")
+            else:
+                count_val = hex(require_byte).replace("0x", "")
             return count_val
     except Exception as e:
-        logger.error(f"error occurred in find_count_register as : {e}")
+        logger.error(f"error occurred in find_byte_count_register as : {e}")
 
 
 # following function is used to decoding receive data for calculation
 def decode_data_fun(received_data):
+    global flag_for_csv
     try:
         slave_id = received_data[0:2]  # here we find slave_id from received data
         function_code = received_data[2:4]  # here we find function_code from received data
         st_add = received_data[4:8]  # here we find starting_address from received data
         mod_count = received_data[8:12]
-        count = find_count_register(mod_count, function_code)  # here we find byte_count from received data
-        start_add = find_start_add_value_from_redis(st_add, function_code, mod_count)  # here we call function for give addresses
+        count = find_byte_count_register(mod_count, function_code)  # here we find byte_count from received data
+        if flag_for_csv == 1:
+            start_add = find_start_add_value_from_csv(st_add, function_code, mod_count)  # here we call function for give addresses from csv file
+        else:
+            start_add = find_start_add_value_from_redis(st_add, function_code, mod_count)  # here we call function for give addresses from redis database
         data = f"{slave_id}{function_code}{count}{start_add}"
         checksum = crc16(data).replace(" ", "0")  # here we find crc16(checksum) for sending data
         return f"{data}{checksum}"
--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from create_mod_slave import read_csv_file as data_from_csv_file
 from create_mod_slave import decode_data_fun
-from create_mod_slave import redis_database as data_from_redis_database
 from loguru import logger
 import serial
 
-# # following object is used to for serial communication
-# serial_reader = serial.Serial(
-#      port='/dev/ttyUSB0',
-#      baudrate=9600,
-#      parity=serial.PARITY_NONE,
-#      stopbits=serial.STOPBITS_ONE,
-#      bytesize=serial.EIGHTBITS
-#      )
+# following object is used to for serial communication
+serial_reader = serial.Serial(
+     port='/dev/ttyUSB0',
+     baudrate=9600,
+     parity=serial.PARITY_NONE,
+     stopbits=serial.STOPBITS_ONE,
+     bytesize=serial.EIGHTBITS
+     )
 
 logger.info(f"Program is started ...")
 logger.info(f"Press 1 for Give Data from CSV file")
 logger.info(f"Press 2 for Give Data from Redis Database")
-# user_choice = int(input(f"Please Enter Your Choice"))
-user_choice = 1
+user_choice = int(input(f"Please Enter Your Choice"))
+# user_choice = 1
 if user_choice == 1:
     data_from_csv_file()
+elif user_choice == 2:
+    pass
 else:
     logger.debug(f"Please Enter valid choice ...")
 
@@ -27,8 +28,8 @@ else:
 if __name__ == '__main__':
     while 1:
         try:
-            # received_data = serial_reader.read(16)
-            received_data = b'\x01\x03\x00\x0a\x00\x04\x64\x0b'
+            received_data = serial_reader.read(16)
+            # received_data = b'\x01\x03\x00\x0a\x00\x08\x64\x0b'
             if len(received_data) > 0:
                 try:
                     received_data = received_data.hex()
@@ -38,7 +39,7 @@ if __name__ == '__main__':
 
                 logger.info(f"Received Data From Modbus : {received_data.upper()}")
                 send_data = bytes.fromhex(decode_data_fun(received_data))
-                # serial_reader.write(send_data)
+                serial_reader.write(send_data)
                 logger.info(f"Send Data To Modbus : {send_data.hex().upper()}")
             else:
                 logger.debug(f"No Data Found")
@@ -48,5 +49,5 @@ if __name__ == '__main__':
             logger.error(f"Found KeyBoard Interrupt,so EXIT Code")
 
         finally:
-            # serial_reader.flush()
+            serial_reader.flush()
             pass
